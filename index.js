ca0521cb4e649f990fce487ed7e4ebf7a6590aef
Josephine Opar <josephineopar@gmail.com>
2023-01-05 18:14:42 +0200

    commit tree 387795c22dc81869429c08688ba3600cb0e20e36

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// function writeCards(["Charlie", "Samip", "Ali"], "birthday") {
+function writeCards(namesOfFriends, event = "birthday") {
+    let messages = []
+    for (let i = 0; i < namesOfFriends.length; i = i + 1) {
+        messages.push(`Thank you, ${namesOfFriends[i]}, for the wonderful ${event} gift!`)
+    }
+    // console.log(messages)
+    return messages
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday")
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        n=n-1
+    }
+}
